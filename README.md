3e8b6b297e659fc333a97af57faeaa6fd5808f41
William Vagharfard <williamv1217@gmail.com>
2020-09-03 09:34:40 -0700

    update readme and fix print

--- README.md
+++ README.md
@@ -1,65 +1,34 @@
 # timesince README
 
-This is the README for your extension "timesince". After writing up a brief description, we recommend including the following sections.
+Use TimeSince to see how long has passed since you last did a task.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
-
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+Set the name of your last task. You can also set the limit for "good", "average", and "bad" times.
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+VS Code
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
 This extension contributes the following settings:
 
-- `myExtension.enable`: enable/disable this extension
-- `myExtension.thing`: set to `blah` to do something
+- `timesince.sinceName`: name of your task
+- `timesince.useTerminalColors`: use default terminal colors for text output
+- `timesince.textColor.green`: set color for green text
+- `timesince.textColor.yellow`: set color for yellow text
+- `timesince.textColor.red`: set color for red text
+- `timesince.limitTime.good`: set time limit for good time
+- `timesince.limitTime.average`: set time limit for average time
+- `timesince.limitTime.bad`: set time limit for bad time
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
+### 0.1.0
 
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
+Initial release
 
 ---
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-- Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-- Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-- Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-- [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-- [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
--- src/Timer.ts
+++ src/Timer.ts
@@ -13,31 +13,6 @@ class Timer {
     this.statusBarItem.command = "timesince.resetTimer";
   }
 
-  private getColor(time: number) {
-    var green;
-    var red;
-    var yellow;
-    let config = vscode.workspace.getConfiguration("timesince");
-
-    if (config.textColor.useTerminalColors) {
-      green = new vscode.ThemeColor("terminal.ansiGreen");
-      red = new vscode.ThemeColor("terminal.ansiRed");
-      yellow = new vscode.ThemeColor("terminal.ansiYellow");
-    } else {
-      green = config.textColor.green;
-      red = config.textColor.red;
-      yellow = config.textColor.yellow;
-    }
-
-    if (time <= 20) {
-      return green;
-    } else if (time <= 40) {
-      return yellow;
-    } else {
-      return red;
-    }
-  }
-
   startTimer() {
     if (this.running) {
       console.log("Already Running!");
@@ -62,11 +37,35 @@ class Timer {
     }, 1000);
   }
 
-  getText(hours: number, minutes: number, seconds: number) {
-    let config = vscode.workspace.getConfiguration("timesince");
+  stopTimer() {
+    console.log("stopping the timer");
+    clearInterval(this.timer);
+    this.running = false;
+    this.statusBarItem.hide();
+  }
 
+  resetTimer() {
+    console.log("resetting the timer");
+    this.stopTimer();
+    this.startTimer();
+  }
+
+  private getTime(startingTime: string) {
+    let currentTime = new Date().toString();
+    let totalTime = Date.parse(currentTime) - Date.parse(startingTime);
+    let hours = Math.floor((totalTime / 1000 / 3600) % 60);
+    let minutes = Math.floor((totalTime / 1000 / 60) % 60);
+    let seconds = Math.floor((totalTime / 1000) % 60);
+
+    return { hours, minutes, seconds };
+  }
+
+  private getText(hours: number, minutes: number, seconds: number) {
+    let config = vscode.workspace.getConfiguration("timesince");
     let baseText = `Last ${config.sinceName}:  `;
-    this.statusBarItem.color = this.getColor(seconds);
+    let hoursToMins = hours * 60;
+
+    this.statusBarItem.color = this.getColor(hoursToMins + minutes);
 
     var hourText = "";
     var minuteText = "";
@@ -95,7 +94,7 @@ class Timer {
     var changedText = "";
 
     if (hours >= 1 && minutes >= 1) {
-      changedText = `${hours} ${hourText} ${minutes} ${minuteText}} ago`;
+      changedText = `${hours} ${hourText} ${minutes} ${minuteText} ago`;
     } else if (hours >= 1) {
       changedText = `${hours} ${hourText} ago`;
     } else if (minutes >= 1) {
@@ -103,31 +102,32 @@ class Timer {
     } else {
       changedText = `${seconds} seconds ago`;
     }
-
     return baseText + changedText;
   }
 
-  getTime(startingTime: string) {
-    let currentTime = new Date().toString();
-    let totalTime = Date.parse(currentTime) - Date.parse(startingTime);
-    let hours = Math.floor((totalTime / 1000 / 3600) % 60);
-    let minutes = Math.floor((totalTime / 1000 / 60) % 60);
-    let seconds = Math.floor((totalTime / 1000) % 60);
+  private getColor(minutes: number) {
+    var green;
+    var red;
+    var yellow;
+    let config = vscode.workspace.getConfiguration("timesince");
 
-    return { hours, minutes, seconds };
-  }
+    if (config.textColor.useTerminalColors) {
+      green = new vscode.ThemeColor("terminal.ansiGreen");
+      red = new vscode.ThemeColor("terminal.ansiRed");
+      yellow = new vscode.ThemeColor("terminal.ansiYellow");
+    } else {
+      green = config.textColor.green;
+      red = config.textColor.red;
+      yellow = config.textColor.yellow;
+    }
 
-  stopTimer() {
-    console.log("stopping the timer");
-    clearInterval(this.timer);
-    this.running = false;
-    this.statusBarItem.hide();
-  }
-
-  resetTimer() {
-    console.log("resetting the timer");
-    this.stopTimer();
-    this.startTimer();
+    if (minutes <= config.limitTime.good) {
+      return green;
+    } else if (minutes <= config.limitTime.average) {
+      return yellow;
+    } else {
+      return red;
+    }
   }
 }
 
